45987d5d93ca5244bb2973990b50fee03e4dd45b
Diego van Haaster <diegomvh@gmail.com>
2018-05-05 09:14:25 -0300

    Fix: public_api imports

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of angular-odata
  */
 
-export * from './lib/angular-odata.service';
-export * from './lib/angular-odata.component';
-export * from './lib/angular-odata.module';
+export * from './lib/odata.module';
