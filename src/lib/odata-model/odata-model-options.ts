66967cd9d85b931ed8882f4386acc3f4a404f535
Diego van Haaster <diegomvh@gmail.com>
2019-07-19 19:05:41 -0300

    Model options

--- src/lib/odata-model/odata-model-options.ts
+++ src/lib/odata-model/odata-model-options.ts
@@ -1,9 +1,11 @@
-import { EventEmitter } from "@angular/core";
-
 export class ODataModelOptions<M> {
   fields: any[];
   relations: any[];
   defaults: any;
+
+  constructor(opts: {fields: any[], relations?: any[], defaults?: any}) {
+    Object.assign(this, {relations: [], defaults: {}}, opts);
+  }
 
   attrs(value: any, parse: boolean) {
     let attrs = parse ? this.parse(value) : Object.create(value);
--- src/lib/odata-model/odata-model.ts
+++ src/lib/odata-model/odata-model.ts
@@ -4,25 +4,25 @@ import { map } from 'rxjs/operators';
 import { ODataEntityService } from '../odata-service/odata-entity.service';
 
 export abstract class ODataModel {
-  protected abstract _meta(): ODataModelOptions<ODataModel>;
-  protected _service: ODataEntityService<ODataModel>;
+  protected abstract meta(): ODataModelOptions<ODataModel>;
+  protected service: ODataEntityService<ODataModel>;
 
   constructor(value: any, parse: boolean = true) {
-    Object.assign(this, this._meta().attrs(value, parse));
+    Object.assign(this, this.meta().attrs(value, parse));
   }
 
   toJSON() {
-    return this._meta().json(this);
+    return this.meta().json(this);
   }
 
   save(parse: boolean = true) : Observable<ODataModel> {
-    return this._service.save(this.toJSON())
+    return this.service.save(this.toJSON())
       .pipe(
-        map(attrs => Object.assign(this, this._meta().attrs(attrs, parse)))
+        map(attrs => Object.assign(this, this.meta().attrs(attrs, parse)))
       );
   }
 
   destroy() : Observable<any> {
-    return this._service.destroy(this.toJSON());
+    return this.service.destroy(this.toJSON());
   }
 }
