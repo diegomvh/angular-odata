08434f20d9a273fb474bea9efa09f20121ee43e0
Diego van Haaster <diegomvh@gmail.com>
2019-08-03 12:37:34 -0300

    Version 0.1

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,23 @@
  * Public API Surface of angular-odata
  */
 
-export * from './lib/angular-odata.service';
-export * from './lib/angular-odata.component';
-export * from './lib/angular-odata.module';
+export * from './lib/odata.module';
+export * from './lib/odata-context';
+
+// Queries
+export * from './lib/odata-query/odata-query-base';
+export * from './lib/odata-query/odata-query';
+export * from './lib/odata-query/odata-query-builder';
+
+// Response
+export * from './lib/odata-response/entity-collection';
+export * from './lib/odata-response/odata-response';
+
+// Models
+export * from './lib/odata-model/odata-model';
+export * from './lib/odata-model/odata-collection';
+
+// Services
+export * from './lib/odata-service/odata.service';
+export * from './lib/odata-service/odata-entity.service';
+export * from './lib/odata-service/odata-model.service';
