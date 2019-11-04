f28bc1ff5a42c7887e7b08486422370cfe95b30f
Diego van Haaster <diegomvh@gmail.com>
2019-11-04 14:08:19 -0300

    Complex Schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,6 +1,6 @@
 import { ODataSettings } from './settings';
 import { Types, Enums } from './utils';
-import { ODataResource, ODataNavigationPropertyResource, ODataPropertyResource, ODataEntityResource, ODataEntitySetResource } from './resources/requests';
+import { ODataResource, ODataEntityResource } from './resources/requests';
 import { PlainObject } from './types';
 
 export interface Field {
@@ -21,6 +21,7 @@ export interface Field {
 }
 
 export interface Parser<T> {
+  type: string;
   parse(value: any, query?: ODataResource<any>): T;
   toJSON(value: T): any;
   parser<E>(name: string): Parser<E>;
@@ -69,8 +70,8 @@ class SchemaField<T> implements Field, Parser<T> {
         (query as ODataEntityResource<any>).navigationProperty<any>(this.name) :
         (query as ODataEntityResource<any>).property<any>(this.name);
       value = (Array.isArray(value) && this.isCollection) ?
-        value.map(v => this.schema.parse(v, query)) :
-        this.schema.parse(value, query);
+        value.map(v => this.schema.parse(v, query.clone<any>())) :
+        this.schema.parse(value, query.clone<any>());
       if (this.model) {
         value = (Array.isArray(value) && this.isCollection) ?
           value.map(v => new this.model(v, query.clone<any>())) :
@@ -115,19 +116,21 @@ class SchemaField<T> implements Field, Parser<T> {
 }
 
 export class Schema<Type> implements Parser<Type> {
-  private fields: { [name: string]: SchemaField<any> };
+  type: string;
+  private fields: SchemaField<any>[];
   model?: { new(...any): any };
 
   constructor(fields?: { [name: string]: Field }) {
     this.fields = Object.entries(fields || {})
-      .reduce((acc, [name, f]) => Object.assign(acc, { [name]: new SchemaField(name, f) }), {});
+      .map(([name, f]) => new SchemaField(name, f));
   }
 
   configure(type: string, settings: ODataSettings) {
-    if (type in settings.models) {
-      this.model = settings.models[type];
+    this.type = type;
+    if (this.type in settings.models) {
+      this.model = settings.models[this.type];
     }
-    Object.values(this.fields).forEach(f => {
+    this.fields.forEach(f => {
       if (f.type in settings.enums) {
         f.enum = settings.enums[f.type];
         f.enumString = settings.stringAsEnum;
@@ -145,9 +148,9 @@ export class Schema<Type> implements Parser<Type> {
   }
 
   toJSON(objs: any): any {
-    let _toJSON = (obj) => Object.assign(obj, Object.entries(this.fields)
-      .filter(([name, f]) => name in obj)
-      .reduce((acc, [name, f]) => Object.assign(acc, { [name]: f.toJSON(obj[name]) }), {})
+    let _toJSON = (obj) => Object.assign(obj, this.fields
+      .filter(f => f.name in obj)
+      .reduce((acc, f) => Object.assign(acc, { [f.name]: f.toJSON(obj[f.name]) }), {})
     );
     return Array.isArray(objs) ? 
       objs.map(obj => _toJSON(obj)) :
@@ -156,16 +159,17 @@ export class Schema<Type> implements Parser<Type> {
 
   parse(objs: any, query?: ODataResource<any>): any {
     let _parse = (obj, q) =>
-      Object.assign(obj, Object.entries(this.fields)
-        .filter(([name, f]) => name in obj)
-        .reduce((acc, [name, f]) => Object.assign(acc, { [name]: f.parse(obj[name], q) }), {})
+      Object.assign(obj, this.fields
+        .filter(f => f.name in obj)
+        .reduce((acc, f) => Object.assign(acc, { [f.name]: f.parse(obj[f.name], q) }), {})
       );
     return Array.isArray(objs) ?
       objs.map(obj => {
-        let entityQuery = (query as ODataEntitySetResource<any>).entity(obj);
-        let attrs = _parse(obj, entityQuery);
+        if (!this.isComplex())
+          (query as ODataEntityResource<any>).key(obj);
+        let attrs = _parse(obj, query);
         return (this.model) ?
-          new this.model(attrs, entityQuery) :
+          new this.model(attrs, query) :
           attrs;
       }) :
       _parse(objs, query);
@@ -176,15 +180,19 @@ export class Schema<Type> implements Parser<Type> {
   }
 
   resolveKey(attrs: any) {
-    let keys = Object.entries(this.fields)
-      .filter(([name, f]) => f.isKey)
-      .map(([name, f]) => [name, f.resolve(attrs)]);
+    let keys = this.fields
+      .filter(f => f.isKey)
+      .map(f => [f.name, f.resolve(attrs)]);
     let key = keys.length === 1 ?
       keys[0][1] :
       keys.reduce((acc, key) => Object.assign(acc, { [key[0]]: key[1] }), {});
     if (!Types.isEmpty(key))
       return key;
     return attrs;
+  }
+
+  isComplex() {
+    return this.fields.every(f => !f.isKey);
   }
 
   /*
