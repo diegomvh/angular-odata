0034d511cbd5352e8241edb4d92c248f8f7564f3
Diego van Haaster <diegomvh@gmail.com>
2019-11-26 22:57:38 -0300

    Annotations

--- src/lib/resources/responses/annotations.ts
+++ src/lib/resources/responses/annotations.ts
@@ -1,4 +1,5 @@
-import { ODATA_ETAG, ODATA_COUNT, ODATA_NEXTLINK, ODATA_ANNOTATION_PREFIX } from '../../types';
+import { ODATA_ETAG, ODATA_COUNT, ODATA_NEXTLINK, ODATA_ANNOTATION_PREFIX, ODATA_TYPE, ODATA_DELTALINK, ODATA_METADATAETAG, ODATA_MEDIA_EDITLINK, ODATA_MEDIA_ETAG, ODATA_MEDIA_READLINK, ODATA_MEDIA_CONTENTTYPE } from '../../types';
+import { ODATA_CONTEXT, ODATA_ID, ODATA_EDITLINK, ODATA_READLINK, ODATA_ASSOCIATIONLINK, ODATA_NAVIGATIONLINK } from 'angular-odata/lib';
 
 /*
   _newResourceForContext(resource: ODataResource<any>, attrs: any) {
@@ -25,24 +26,75 @@ import { ODATA_ETAG, ODATA_COUNT, ODATA_NEXTLINK, ODATA_ANNOTATION_PREFIX } from
   */
 
 export class ODataAnnotations {
+  value: {[name: string]: string | number}
   constructor(body: any) {
-    Object.keys(body)
-      .filter(key => key.startsWith(ODATA_ANNOTATION_PREFIX))
-      .forEach(key => {
-        this[key] = body[key];
-      });
+    this.value = Object.keys(body)
+      .filter(key => key.indexOf(ODATA_ANNOTATION_PREFIX) !== -1)
+      .reduce((acc, key) => Object.assign(acc, {[key]: body[key]}), {});
+  }
+
+  get context(): string {
+    return this.value[ODATA_CONTEXT] as string;
+  }
+
+  get id(): string {
+    return this.value[ODATA_ID] as string;
+  }
+
+  get type(): string {
+    return this.value[ODATA_TYPE] as string;
   }
 
   get etag(): string {
-    return this[ODATA_ETAG] as string;
+    return this.value[ODATA_ETAG] as string;
+  }
+
+  get metadataEtag(): string {
+    return this.value[ODATA_METADATAETAG] as string;
+  }
+
+  get mediaEtag(): string {
+    return decodeURIComponent(this.value[ODATA_MEDIA_ETAG] as string);
   }
 
   get count(): number {
-    return this[ODATA_COUNT] as number;
+    return this.value[ODATA_COUNT] as number;
   }
 
   get nextLink(): string {
-    return decodeURIComponent(this[ODATA_NEXTLINK]) as string;
+    return decodeURIComponent(this.value[ODATA_NEXTLINK] as string);
+  }
+
+  get deltaLink(): string {
+    return decodeURIComponent(this.value[ODATA_DELTALINK] as string);
+  }
+
+  get readLink(): string {
+    return decodeURIComponent(this.value[ODATA_READLINK] as string);
+  }
+
+  get editLink(): string {
+    return decodeURIComponent(this.value[ODATA_EDITLINK] as string);
+  }
+
+  get associationLink(): string {
+    return decodeURIComponent(this.value[ODATA_ASSOCIATIONLINK] as string);
+  }
+
+  get navigationLink(): string {
+    return decodeURIComponent(this.value[ODATA_NAVIGATIONLINK] as string);
+  }
+
+  get mediaReadLink(): string {
+    return decodeURIComponent(this.value[ODATA_MEDIA_READLINK] as string);
+  }
+
+  get mediaEditLink(): string {
+    return decodeURIComponent(this.value[ODATA_MEDIA_EDITLINK] as string);
+  }
+
+  get mediaContentType(): string {
+    return this.value[ODATA_MEDIA_CONTENTTYPE] as string;
   }
 
   get skip(): number {
