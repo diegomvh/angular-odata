6910dd6b66e3b97cd067de1b2b83e5af2ce1f4e5
Diego van Haaster <diegomvh@gmail.com>
2020-02-26 09:45:57 -0300

    Callable

--- src/lib/resources/requests/action.ts
+++ src/lib/resources/requests/action.ts
@@ -4,13 +4,10 @@ import { Observable } from 'rxjs';
 import { ODataSegments, Segments } from '../segments';
 import { ODataOptions } from '../options';
 import { ODataClient } from '../../client';
-import { ODataResource } from '../resource';
 import { Parser } from '../../models';
-import { map } from 'rxjs/operators';
-import { $COUNT } from '../../types';
-import { ODataEntityAnnotations, ODataCollectionAnnotations, ODataPropertyAnnotations } from '../responses';
+import { ODataCallableResource } from './callable';
 
-export class ODataActionResource<T> extends ODataResource<T> {
+export class ODataActionResource<T> extends ODataCallableResource<T> {
   // Factory
   static factory<R>(name: string, client: ODataClient, opts?: {
       segments?: ODataSegments, 
@@ -24,64 +21,5 @@ export class ODataActionResource<T> extends ODataResource<T> {
     segments.segment(Segments.actionCall, name);
     options.clear();
     return new ODataActionResource<R>(client, segments, options, parser);
-  }
-
-  post(body: any | null, options: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    reportProgress?: boolean,
-    responseType?: 'entity',
-    withCredentials?: boolean,
-  }): Observable<[T, ODataEntityAnnotations]>;
-
-  post(body?: any | null, options?: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    reportProgress?: boolean,
-    responseType?: 'entities',
-    withCredentials?: boolean,
-    withCount?: boolean
-  }): Observable<[T[], ODataCollectionAnnotations]>;
-
-  post(body?: any | null, options?: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    reportProgress?: boolean,
-    responseType?: 'property',
-    withCredentials?: boolean,
-  }): Observable<[T, ODataPropertyAnnotations]>;
-
-  post(body?: any | null, options?: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    responseType?: 'entity'|'entities'|'property',
-    reportProgress?: boolean,
-    withCredentials?: boolean,
-    withCount?: boolean
-  }): Observable<any> {
-
-    let params = options && options.params;
-    if (options && options.withCount)
-      params = this.client.mergeHttpParams(params, {[$COUNT]: 'true'})
-
-    let res$ = this.client.post(this, body, {
-      headers: options && options.headers,
-      observe: 'body',
-      params: params,
-      responseType: 'json',
-      reportProgress: options && options.reportProgress,
-      withCredentials: options && options.withCredentials
-    });
-    if (options && options.responseType) {
-      switch (options.responseType) {
-        case 'entity':
-          return res$.pipe(map((body: any) => this.toEntity(body)));
-        case 'entities':
-          return res$.pipe(map((body: any) => this.toEntities(body)));
-        case 'property':
-          return res$.pipe(map((body: any) => this.toValue(body)));
-      }
-    }
-    return res$;
   }
 }
--- src/lib/resources/requests/function.ts
+++ src/lib/resources/requests/function.ts
@@ -1,16 +1,11 @@
-import { HttpParams, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-import { ODataResource } from '../resource';
 import { ODataSegments, Segments } from '../segments';
 import { ODataOptions, Options } from '../options';
 import { ODataClient } from '../../client';
-import { PlainObject, $COUNT } from '../../types';
+import { PlainObject } from '../../types';
 import { Parser } from '../../models';
-import { map } from 'rxjs/operators';
-import { ODataEntityAnnotations, ODataCollectionAnnotations, ODataPropertyAnnotations } from '../responses';
+import { ODataCallableResource } from './callable';
 
-export class ODataFunctionResource<T> extends ODataResource<T> {
+export class ODataFunctionResource<T> extends ODataCallableResource<T> {
 
   // Factory
   static factory<R>(name: string, service: ODataClient, opts?: {
@@ -30,64 +25,5 @@ export class ODataFunctionResource<T> extends ODataResource<T> {
   // Parameters
   parameters(opts?: PlainObject) {
     return this.segments.last().option(Options.parameters, opts);
-  }
-
-  get(options?: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    reportProgress?: boolean,
-    responseType?: 'entity',
-    withCredentials?: boolean,
-  }): Observable<[T, ODataEntityAnnotations]>;
-
-  get(options?: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    reportProgress?: boolean,
-    responseType?: 'entities',
-    withCredentials?: boolean,
-    withCount?: boolean
-  }): Observable<[T[], ODataCollectionAnnotations]>;
-
-  get(options?: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    reportProgress?: boolean,
-    responseType?: 'property',
-    withCredentials?: boolean,
-  }): Observable<[T, ODataPropertyAnnotations]>;
-
-  get(options?: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-    params?: HttpParams|{[param: string]: string | string[]},
-    responseType?: 'entity'|'entities'|'property',
-    reportProgress?: boolean,
-    withCredentials?: boolean,
-    withCount?: boolean
-  }): Observable<any> {
-
-    let params = options && options.params;
-    if (options && options.withCount)
-      params = this.client.mergeHttpParams(params, {[$COUNT]: 'true'})
-
-    let res$ = this.client.get<T>(this,{
-      headers: options && options.headers,
-      observe: 'body',
-      params: params,
-      responseType: 'json',
-      reportProgress: options && options.reportProgress,
-      withCredentials: options && options.withCredentials
-    });
-    if (options && options.responseType) {
-      switch (options.responseType) {
-        case 'entity':
-          return res$.pipe(map((body: any) => this.toEntity(body)));
-        case 'entities':
-          return res$.pipe(map((body: any) => this.toEntities(body)));
-        case 'property':
-          return res$.pipe(map((body: any) => this.toValue(body)));
-      }
-    }
-    return res$;
   }
 }
