f34b1603958187d5ab7582877d6820509e2ca576
Diego van Haaster <diegomvh@gmail.com>
2020-05-14 20:45:54 -0300

    Update factory

--- src/lib/services/factory.ts
+++ src/lib/services/factory.ts
@@ -2,15 +2,24 @@ import { Injectable } from '@angular/core';
 
 import { ODataClient } from "../client";
 import { ODataEntityService } from './entity';
+import { ODataModel, ODataCollection } from '../models';
+import { ODataModelService } from './model';
 
 @Injectable()
 export class ODataServiceFactory {
   constructor(protected client: ODataClient) { }
 
-  create<T>(path: string, type?: string): ODataEntityService<T> {
+  createEntityService<T>(path: string, type?: string): ODataEntityService<T> {
     return new class extends ODataEntityService<T> {
       static path = path;
-      static type = type || "";
+      static entity = type || "";
+    }(this.client);
+  }
+
+  createModelService<T, M extends ODataModel<T>, C extends ODataCollection<T, M>>(path: string, type?: string): ODataModelService<T, M, C> {
+    return new class extends ODataModelService<T, M, C> {
+      static path = path;
+      static entity = type || "";
     }(this.client);
   }
 }
