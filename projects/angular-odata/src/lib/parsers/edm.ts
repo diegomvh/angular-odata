216fbb12651576851e22764ba70ccbe9eacabf26
Diego van Haaster <diegomvh@gmail.com>
2020-07-06 15:57:28 -0300

    Edm Parsers

--- projects/angular-odata/src/lib/parsers/edm.ts
+++ projects/angular-odata/src/lib/parsers/edm.ts
@@ -2,62 +2,32 @@ import { Parser } from '../types';
 
 // InMemory Filter!
 // https://github.com/jaystack/odata-v4-inmemory/
-const DATE = <Parser<Date>>{
-  deserialize(value: any): Date {
-    return new Date(value);
+export const DATE_PARSERS: { [type: string]: Parser<any> } = {
+  'Edm.Date': <Parser<Date>>{
+    deserialize(value: any): Date {
+      return new Date(value);
+    },
+    serialize(value: Date): any {
+      return value.toISOString();
+    }
   },
-  serialize(value: Date): any {
-    return value.toISOString();
-  }
-};
-
-export const DATE_PARSER: { [type: string]: Parser<any> } = {
-  'Edm.Date': DATE,
-  'Edm.DateTimeOffset': DATE
-};
-
-export const DECIMAL_PARSER: { [type: string]: Parser<any> } = {
-  'Edm.Decimal': <Parser<number>>{
-    deserialize(value: any): number {
-      if (this.ieee754Compatible) {
-        return Number(value);
-      }
-      return value;
+  'Edm.DateTimeOffset': <Parser<Date>>{
+    deserialize(value: any): Date {
+      return new Date(value);
     },
-    serialize(value: number): any {
-      if (this.ieee754Compatible) {
-        return parseFloat(value.toPrecision(this.precision)).toFixed(this.scale);
-      }
-      return value;
+    serialize(value: Date): any {
+      return value.toISOString();
     }
-  }
-};
-
-export const DOUBLE_PARSER: { [type: string]: Parser<any> } = {
-  'Edm.Double': <Parser<number>>{
-    deserialize(value: any): number {
-      if (value === 'INF') return Infinity;
-      return DECIMAL_PARSER['Decimal'].deserialize(value);
-    },
-    serialize(value: number): any {
-      if (value === Infinity) return 'INF';
-      return DECIMAL_PARSER['Decimal'].serialize(value);
-    }
-  }
-};
-
-export const TIME_PARSER: { [type: string]: Parser<any> } = {
-  'Edm.Time': <Parser<Date>>{
+  },
+  'Edm.TimeOfDay': <Parser<Date>>{
     deserialize(value: any): Date {
       return new Date(`1970-01-01T${value}Z`);
     },
     serialize(value: Date): any {
+      //TODO: serialize Time from Date
       return value.toTimeString();
     }
-  }
-};
-
-export const DURATION_PARSER: { [type: string]: Parser<any> } = {
+  },
   'Edm.Duration': <Parser<Date>>{
     deserialize(value: any): Date {
       var m = value.match(/P([0-9]*D)?T?([0-9]{1,2}H)?([0-9]{1,2}M)?([\.0-9]*S)?/)
@@ -81,9 +51,31 @@ export const DURATION_PARSER: { [type: string]: Parser<any> } = {
   }
 };
 
-export const EDM_PARSERS = Object.assign({},
-  DATE_PARSER,
-  DECIMAL_PARSER,
-  DOUBLE_PARSER,
-  TIME_PARSER,
-  DURATION_PARSER);
+export const NUMBER_PARSERS: { [type: string]: Parser<any> } = {
+  'Edm.Decimal': <Parser<number>>{
+    deserialize(value: any): number {
+      if (this.ieee754Compatible) {
+        return parseFloat(value);
+      }
+      return value;
+    },
+    serialize(value: number): any {
+      if (this.ieee754Compatible) {
+        return parseFloat(value.toPrecision(this.precision)).toFixed(this.scale);
+      }
+      return value;
+    }
+  },
+  'Edm.Double': <Parser<number>>{
+    deserialize(value: any): number {
+      if (value === 'INF') return Infinity;
+      return value;
+    },
+    serialize(value: number): any {
+      if (value === Infinity) return 'INF';
+      return value;
+    }
+  }
+};
+
+export const EDM_PARSERS = Object.assign({}, DATE_PARSERS, NUMBER_PARSERS);
