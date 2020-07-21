51d14d6085db814462050c9d7588e59f178a7814
Diego van Haaster <diegomvh@gmail.com>
2020-07-21 20:22:53 -0300

    Remove lodash functions :P

--- projects/angular-odata/src/lib/utils/types.ts
+++ projects/angular-odata/src/lib/utils/types.ts
@@ -1,10 +1,3 @@
-import { Tags } from './tags';
-
-const MAX_SAFE_INTEGER = 9007199254740991;
-const reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/,
-  reIsPlainProp = /^\w*$/,
-  reIsUint = /^(?:0|[1-9]\d*)$/;
-
 export const Types = {
   isNull(value: any): boolean {
     return value === null;
@@ -27,45 +20,12 @@ export const Types = {
     return value != null && typeof value == 'object';
   },
 
-  isSymbol(value: any): boolean {
-    return typeof value == 'symbol' ||
-      (Types.isObjectLike(value) && Tags.baseGetTag(value) == '[object Symbol]');
-  },
-
   isFunction(value: any): boolean {
     return typeof value === "function";
   },
 
   isArray(value: any): boolean {
     return Array.isArray(value);
-  },
-
-  isLength(value) {
-    return typeof value == 'number' &&
-      value > -1 && value % 1 == 0 && value <= MAX_SAFE_INTEGER;
-  },
-
-  isIndex(value, length?: number) {
-    var type = typeof value;
-    length = length == null ? MAX_SAFE_INTEGER : length;
-
-    return !!length &&
-      (type == 'number' ||
-        (type != 'symbol' && reIsUint.test(value))) &&
-      (value > -1 && value % 1 == 0 && value < length);
-  },
-
-  isKey(value, object) {
-    if (Array.isArray(value)) {
-      return false;
-    }
-    var type = typeof value;
-    if (type == 'number' || type == 'symbol' || type == 'boolean' ||
-      value == null || Types.isSymbol(value)) {
-      return true;
-    }
-    return reIsPlainProp.test(value) || !reIsDeepProp.test(value) ||
-      (object != null && value in Object(object));
   },
 
   isEmpty(value: any): boolean {
--- projects/angular-odata/src/lib/utils/urls.ts
+++ projects/angular-odata/src/lib/utils/urls.ts
@@ -1,10 +1,12 @@
-export const escapeIllegalChars = (string: string) => {
-  string = string.replace(/%/g, '%25');
-  string = string.replace(/\+/g, '%2B');
-  string = string.replace(/\//g, '%2F');
-  string = string.replace(/\?/g, '%3F');
-  string = string.replace(/#/g, '%23');
-  string = string.replace(/&/g, '%26');
-  string = string.replace(/'/g, "''");
-  return string;
+export const Urls = {
+  escapeIllegalChars(string: string) {
+    string = string.replace(/%/g, '%25');
+    string = string.replace(/\+/g, '%2B');
+    string = string.replace(/\//g, '%2F');
+    string = string.replace(/\?/g, '%3F');
+    string = string.replace(/#/g, '%23');
+    string = string.replace(/&/g, '%26');
+    string = string.replace(/'/g, "''");
+    return string;
+  }
 }
