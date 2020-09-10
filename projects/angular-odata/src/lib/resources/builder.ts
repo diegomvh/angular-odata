5227d2152daddb4a74139e3adaed7fc83adaa1de
Diego van Haaster <diegomvh@gmail.com>
2020-09-10 16:31:57 -0300

    Add Duration to query-builder

--- projects/angular-odata/src/lib/resources/builder.ts
+++ projects/angular-odata/src/lib/resources/builder.ts
@@ -51,13 +51,15 @@ export type GroupBy<T> = {
 
 export type Raw = { type: 'raw'; value: any; }
 export type Guid = { type: 'guid'; value: any; }
+export type Duration = { type: 'duration'; value: any; }
 export type Binary = { type: 'binary'; value: any; }
 export type Json = { type: 'json'; value: any; }
 export type Alias = { type: 'alias'; name: string; value: any; }
-export type Value = string | Date | number | boolean | Raw | Guid | Binary | Json | Alias; 
+export type Value = string | Date | number | boolean | Raw | Guid | Duration | Binary | Json | Alias; 
 
 export const raw = (value: string): Raw => ({type: 'raw', value});
 export const guid = (value: string): Guid => ({type: 'guid', value});
+export const duration = (value: string): Duration => ({type: 'duration', value});
 export const binary = (value: string): Binary => ({type: 'binary', value});
 export const json = (value: PlainObject): Json => ({type: 'json', value});
 export const alias = (name: string, value: PlainObject): Alias => ({ type: 'alias', name, value });
@@ -335,6 +337,8 @@ function handleValue(value: Value, aliases?: Alias[]): any {
       return value.value;
     } else if (value.type === 'guid') {
       return value.value;
+    } else if (value.type === 'duration') {
+      return `duration'${value.value}'`;
     } else if (value.type === 'binary') {
       return `binary'${value.value}'`;
     } else if (value.type === 'alias') {
@@ -396,7 +400,7 @@ function buildExpand<T>(expands: Expand<T>): string {
               break;
             case 'count':
             case 'top':
-              value = `${expands[key]}`;
+              value = `${(expands as NestedExpandOptions<any>)[key]}`;
               break;
             default: 
               value = buildExpand((expands as NestedExpandOptions<any>)[key] as Expand<T>);
