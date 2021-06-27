e549a87de0891891dc29850b3793dd2752d1824e
Diego van Haaster <diegomvh@gmail.com>
2021-06-27 08:15:20 -0300

    Clean code

--- projects/angular-odata/src/lib/resources/types/entity-set.ts
+++ projects/angular-odata/src/lib/resources/types/entity-set.ts
@@ -11,40 +11,63 @@ import { ODataEntityResource } from './entity';
 import { ODataCountResource } from './count';
 import { EntityKey, ODataResource } from '../resource';
 import { HttpOptions } from './options';
-import { ODataEntity, ODataEntities, ODataEntitiesAnnotations } from '../responses';
+import {
+  ODataEntity,
+  ODataEntities,
+  ODataEntitiesAnnotations,
+} from '../responses';
 import { ODataModel, ODataCollection } from '../../models';
 import { ODataApi } from '../../api';
 
 export class ODataEntitySetResource<T> extends ODataResource<T> {
   //#region Factory
-  static factory<E>(api: ODataApi, path: string, type: string | undefined, segments: ODataPathSegments, query: ODataQueryOptions) {
-    const segment = segments.add(PathSegmentNames.entitySet, path)
-    if (type)
-      segment.type(type);
+  static factory<E>(
+    api: ODataApi,
+    path: string,
+    type: string | undefined,
+    segments: ODataPathSegments,
+    query: ODataQueryOptions
+  ) {
+    const segment = segments.add(PathSegmentNames.entitySet, path);
+    if (type) segment.type(type);
     return new ODataEntitySetResource<E>(api, segments, query);
   }
   //#endregion
 
   clone() {
-    return new ODataEntitySetResource<T>(this.api, this.cloneSegments(), this.cloneQuery());
+    return new ODataEntitySetResource<T>(
+      this.api,
+      this.cloneSegments(),
+      this.cloneQuery()
+    );
   }
 
   schema() {
     let type = this.type();
-    return (type !== undefined) ?
-      this.api.findStructuredTypeForType<T>(type) :
-      undefined;
+    return type !== undefined
+      ? this.api.findStructuredTypeForType<T>(type)
+      : undefined;
   }
 
-  asCollection<M extends ODataModel<T>, C extends ODataCollection<T, M>>(entities: Partial<T>[] | {[name: string]: any}[], {annots, reset = false}: { annots?: ODataEntitiesAnnotations, reset?: boolean} = {}): C {
+  asCollection<M extends ODataModel<T>, C extends ODataCollection<T, M>>(
+    entities: Partial<T>[] | { [name: string]: any }[],
+    {
+      annots,
+      reset = false,
+    }: { annots?: ODataEntitiesAnnotations; reset?: boolean } = {}
+  ): C {
     const type = annots?.type || this.type();
     const Collection = this.api.collectionForType(type);
-    return new Collection(entities, {resource: this, annots, reset}) as C;
+    return new Collection(entities, { resource: this, annots, reset }) as C;
   }
 
   //#region Inmutable Resource
   entity(key?: any) {
-    const entity = ODataEntityResource.factory<T>(this.api, this.cloneSegments(), this.cloneQuery());
+    const entity = ODataEntityResource.factory<T>(
+      this.api,
+      this.cloneSegments(),
+      this.cloneQuery()
+    );
     if (key !== undefined) {
       return entity.key(key);
     }
@@ -65,7 +88,13 @@ export class ODataEntitySetResource<T> extends ODataResource<T> {
       path = callable.path();
       type = callable.type();
     }
-    return ODataActionResource.factory<P, R>(this.api, path, type, this.cloneSegments(), this.cloneQuery());
+    return ODataActionResource.factory<P, R>(
+      this.api,
+      path,
+      type,
+      this.cloneSegments(),
+      this.cloneQuery()
+    );
   }
 
   function<P, R>(name: string) {
@@ -76,11 +105,21 @@ export class ODataEntitySetResource<T> extends ODataResource<T> {
       path = callable.path();
       type = callable.type();
     }
-    return ODataFunctionResource.factory<P, R>(this.api, path, type, this.cloneSegments(), this.cloneQuery());
+    return ODataFunctionResource.factory<P, R>(
+      this.api,
+      path,
+      type,
+      this.cloneSegments(),
+      this.cloneQuery()
+    );
   }
 
   count() {
-    return ODataCountResource.factory(this.api, this.cloneSegments(), this.cloneQuery());
+    return ODataCountResource.factory(
+      this.api,
+      this.cloneSegments(),
+      this.cloneQuery()
+    );
   }
 
   select(opts: Select<T>) {
@@ -150,8 +189,8 @@ export class ODataEntitySetResource<T> extends ODataResource<T> {
     return {
       entitySet() {
         return segments.get(PathSegmentNames.entitySet);
-      }
-    }
+      },
+    };
   }
 
   get query() {
@@ -160,43 +199,64 @@ export class ODataEntitySetResource<T> extends ODataResource<T> {
   //#endregion
 
   //#region Requests
-  post(attrs: Partial<T>, options: HttpOptions = {}): Observable<ODataEntity<T>> {
-    return super.post(attrs, {responseType: 'entity', ...options});
+  post(
+    attrs: Partial<T>,
+    options: HttpOptions = {}
+  ): Observable<ODataEntity<T>> {
+    return super.post(attrs, { responseType: 'entity', ...options });
   }
 
-  get(options: HttpOptions & { withCount?: boolean } = {}): Observable<ODataEntities<T>> {
-    return super.get({responseType: 'entities', ...options});
+  get(
+    options: HttpOptions & { withCount?: boolean } = {}
+  ): Observable<ODataEntities<T>> {
+    return super.get({ responseType: 'entities', ...options });
   }
   //#endregion
 
   //#region Shortcuts
-  fetch(options?: HttpOptions & { withCount?: boolean }): Observable<ODataEntities<T>> {
+  fetch(
+    options?: HttpOptions & { withCount?: boolean }
+  ): Observable<ODataEntities<T>> {
     return this.get(options);
   }
 
-  fetchEntities(options?: HttpOptions & { withCount?: boolean }): Observable<T[] | null> {
-    return this.fetch(options).pipe(map(({entities}) => entities));
+  fetchEntities(
+    options?: HttpOptions & { withCount?: boolean }
+  ): Observable<T[] | null> {
+    return this.fetch(options).pipe(map(({ entities }) => entities));
   }
 
-  fetchCollection(options?: HttpOptions & { withCount?: boolean }): Observable<ODataCollection<T, ODataModel<T>> | null> {
-    return this.fetch(options).pipe(map(({entities, annots}) => entities ? this.asCollection(entities, { annots, reset: true}) : null));
+  fetchCollection(
+    options?: HttpOptions & { withCount?: boolean }
+  ): Observable<ODataCollection<T, ODataModel<T>> | null> {
+    return this.fetch(options).pipe(
+      map(({ entities, annots }) =>
+        entities ? this.asCollection(entities, { annots, reset: true }) : null
+      )
+    );
   }
 
   fetchAll(options?: HttpOptions): Observable<T[]> {
     let res = this.clone();
     // Clean Paging
     res.query.clearPaging();
-    let fetch = (opts?: { skip?: number, skiptoken?: string, top?: number }): Observable<ODataEntities<T>> => {
+    let fetch = (opts?: {
+      skip?: number;
+      skiptoken?: string;
+      top?: number;
+    }): Observable<ODataEntities<T>> => {
       if (opts) {
         res.query.paging(opts);
       }
       return res.get(options);
-    }
-    return fetch()
-      .pipe(
-        expand(({annots: meta})  => (meta.skip || meta.skiptoken) ? fetch(meta) : EMPTY),
-        concatMap(({entities}) => entities || []),
-        toArray());
+    };
+    return fetch().pipe(
+      expand(({ annots: meta }) =>
+        meta.skip || meta.skiptoken ? fetch(meta) : EMPTY
+      ),
+      concatMap(({ entities }) => entities || []),
+      toArray()
+    );
   }
   //#endregion
 }
