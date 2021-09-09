23268308ea12b3f899d80d5fc84c90a199e4dc04
Diego van Haaster <diegomvh@gmail.com>
2021-09-09 09:47:51 -0300

    Enuums tests

---
+++ projects/angular-odata/src/lib/utils/enums.spec.ts
@@ -0,0 +1,8 @@
+import { TestBed } from '@angular/core/testing';
+import { Enums } from './enums';
+
+describe('Enums', () => {
+  it('Enums tests', () => {
+    expect(undefined).toBeUndefined();
+  });
+});
