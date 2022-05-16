d8342c5e3d6d5117d4788267f66979ea03e8d3c5
Diego van Haaster <diegomvh@gmail.com>
2022-05-16 13:58:49 -0300

    Docstrings

--- projects/angular-odata/src/lib/resources/query/handlers.ts
+++ projects/angular-odata/src/lib/resources/query/handlers.ts
@@ -29,19 +29,35 @@ export class ODataQueryOptionHandler<T> {
     private n: QueryOptionNames
   ) {}
 
+  /**
+   * The name of the managed odata query option.
+   */
   get name() {
     return this.n;
   }
 
+  /**
+   * Converts the managed odata query option to a json object.
+   * @returns {any}
+   */
   toJSON() {
     return this.o.get(this.n);
   }
 
+  /**
+   * Returns a boolean indicating if the managed odata query option is empty. 
+   * @returns True if the managed odata query option is empty.
+   */
   empty() {
     return Types.isEmpty(this.o.get(this.n));
   }
 
   //#region Primitive Value
+  /**
+   * Get or Set the value of the managed odata query option.
+   * @param v The value to set.
+   * @returns 
+   */
   value(v?: any) {
     if (v !== undefined) this.o.set(this.n, v);
     return this.o.get(this.n);
@@ -55,10 +71,18 @@ export class ODataQueryOptionHandler<T> {
     return this.o.get(this.n);
   }
 
+  /**
+   * Push value to the managed odata query option. 
+   * @param value Value to push
+   */
   push(value: any) {
     this.assertArray().push(value);
   }
 
+  /**
+   * Remove value from the managed odata query option. 
+   * @param value Value to remove 
+   */
   remove(value: any) {
     this.o.set(
       this.n,
@@ -69,6 +93,11 @@ export class ODataQueryOptionHandler<T> {
       this.o.set(this.n, this.o.get(this.n)[0]);
   }
 
+  /**
+   * Return value at index of the managed odata query option. 
+   * @param index Index of the value
+   * @returns The value
+   */
   at(index: number) {
     return this.assertArray()[index];
   }
@@ -91,16 +120,31 @@ export class ODataQueryOptionHandler<T> {
     return obj;
   }
 
+  /**
+   * Set the value for path in the managed odata query option. 
+   * @param path Path for set the value
+   * @param value Value to set
+   */
   set(path: string, value: any) {
     let obj = this.assertObject(true);
     Objects.set(obj, path, value);
   }
 
+  /**
+   * Get the value for path from the managed odata query option. 
+   * @param path The path from get the value
+   * @param def Default if not found
+   * @returns 
+   */
   get(path: string, def?: any): any {
     let obj = this.assertObject(false) || {};
     return Objects.get(obj, path, def);
   }
 
+  /**
+   * Unset the value for path in the managed odata query option. 
+   * @param path 
+   */
   unset(path: string) {
     let obj = this.assertObject(true);
     Objects.unset(obj, path);
@@ -115,17 +159,30 @@ export class ODataQueryOptionHandler<T> {
     }
   }
 
+  /**
+   * Test if the managed odata query option has the value.
+   * @param path The path fot test if the value is set
+   * @returns Boolean indicating if the value is set
+   */
   has(path: string) {
     let obj = this.assertObject(false) || {};
     return Objects.has(obj, path);
   }
 
+  /**
+   * Merge values from object into the managed odata query option.
+   * @param values Object to merge
+   * @returns 
+   */
   assign(values: { [attr: string]: any }) {
     let obj = this.assertObject(true);
     return Objects.merge(obj, values);
   }
   //#endregion
 
+  /**
+   * Clear the managed odata query option.
+   */
   clear() {
     this.o.delete(this.n);
   }
@@ -154,6 +211,11 @@ export class ODataQueryOptionsHandler<T> {
     return normalizeValue(value);
   }
 
+  /**
+   * Build and return a handler for modifying the $select option.
+   * If opts is given then set te value as new value for $select. 
+   * @param opts Select<T> value or builder function for SelectExpression<T>
+   */
   select(
     opts: (
       builder: { s: T; e: () => SelectExpression<T> },
@@ -175,6 +237,11 @@ export class ODataQueryOptionsHandler<T> {
     return this.options.option<Select<T>>(QueryOptionNames.select, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $expand option. 
+   * If opts is given then set te value as new value for $expand.
+   * @param opts Expand<T> value or builder function for ExpandExpression<T>
+   */
   expand(
     opts: (
       builder: { s: T; e: () => ExpandExpression<T> },
@@ -197,7 +264,10 @@ export class ODataQueryOptionsHandler<T> {
   }
 
   /**
+   * Build and return a handler for modifying the $compute option. 
+   * If opts is given then set te value as new value for $compute.
    * @link https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_SystemQueryOptioncompute
+   * @param opts string value or builder function for ComputeExpression<T>
    */
   compute(
     opts: (
@@ -226,7 +296,10 @@ export class ODataQueryOptionsHandler<T> {
   }
 
   /**
+   * Build and return a handler for modifying the $format option. 
+   * If opts is given then set te value as new value for $format.
    * @link https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_SystemQueryOptionformat
+   * @param opts string value for format 
    */
   format(opts: string): ODataQueryOptionHandler<T>;
   format(): ODataQueryOptionHandler<T>;
@@ -234,12 +307,22 @@ export class ODataQueryOptionsHandler<T> {
     return this.options.option<string>(QueryOptionNames.format, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $transform option. 
+   * If opts is given then set te value as new value for $transform.
+   * @param opts string value for transform
+   */
   transform(opts: Transform<T>): ODataQueryOptionHandler<T>;
   transform(): ODataQueryOptionHandler<T>;
   transform(opts?: Transform<T>): any {
     return this.options.option<Transform<T>>(QueryOptionNames.transform, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $search option.
+   * If opts is given then set te value as new value for $search.
+   * @param opts string value or builder function for SearchExpression<T>
+   */
   search(
     opts: (
       builder: {
@@ -263,6 +346,11 @@ export class ODataQueryOptionsHandler<T> {
     return this.options.option<string>(QueryOptionNames.search, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $filter option. 
+   * If opts is given then set te value as new value for $filter.
+   * @param opts Filter<T> value or builder function for FilterExpression<T>
+   */
   filter(
     opts: (
       builder: {
@@ -289,6 +377,11 @@ export class ODataQueryOptionsHandler<T> {
     return this.options.option<Filter<T>>(QueryOptionNames.filter, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $orderby option. 
+   * If opts is given then set te value as new value for $orderby.
+   * @param opts OrderBy<T> value or builder function for OrderByExpression<T>
+   */
   orderBy(
     opts: (
       builder: { s: T; e: () => OrderByExpression<T> },
@@ -310,24 +403,43 @@ export class ODataQueryOptionsHandler<T> {
     return this.options.option<OrderBy<T>>(QueryOptionNames.orderBy, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $top option.
+   * If opts is given then set te value as new value for $top.
+   * @param opts number value
+   */
   top(opts: number): ODataQueryOptionHandler<T>;
   top(): ODataQueryOptionHandler<T>;
   top(opts?: number): any {
     return this.options.option<number>(QueryOptionNames.top, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $skip option.
+   * If opts is given then set te value as new value for $skip.
+   * @param opts number value
+   */
   skip(opts: number): ODataQueryOptionHandler<T>;
   skip(): ODataQueryOptionHandler<T>;
   skip(opts?: number): any {
     return this.options.option<number>(QueryOptionNames.skip, opts);
   }
 
+  /**
+   * Build and return a handler for modifying the $skiptoken option.
+   * If opts is given then set te value as new value for $skiptoken.
+   * @param opts string value
+   */
   skiptoken(opts: string): ODataQueryOptionHandler<T>;
   skiptoken(): ODataQueryOptionHandler<T>;
   skiptoken(opts?: string): any {
     return this.options.option<string>(QueryOptionNames.skiptoken, opts);
   }
 
+  /**
+   * Shortcut for set $top, $skip, $skiptoken.
+   * @param param0 skip or top or skiptoken
+   */
   paging({
     skip,
     skiptoken,
@@ -338,12 +450,19 @@ export class ODataQueryOptionsHandler<T> {
     if (top !== undefined) this.top(top);
   }
 
+  /**
+   * Shortcut for clear pagination by unset $top, $skip, $skiptoken. 
+   */
   clearPaging() {
     this.skip().clear();
     this.top().clear();
     this.skiptoken().clear();
   }
 
+  /**
+   * Apply the given query options to the current query.
+   * @param query The query to be applied.
+   */
   apply(query: ODataQueryArguments<T>) {
     if (query.select !== undefined) {
       this.options.option(QueryOptionNames.select, query.select);
