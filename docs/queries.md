41a575075c544fa38477e293b322a5ef16f28305
Diego van Haaster <dvanhaaster@juschubut.gov.ar>
2025-05-26 10:13:35 -0300

    Fix ODataApiConfig Name

---
+++ docs/queries.md
@@ -0,0 +1,8 @@
+# Queries
+
+## Query Builder
+
+For a deep query customizations the library use `odata-query` and `odata-filter-builder` as a builders.
+
+- [OData v4 query builder](https://github.com/techniq/odata-query)
+- [OData Filter Builder](https://github.com/bodia-uz/odata-filter-builder)
--- projects/angular-odata/schematics/apigen/files/api-config/__fileName__.ts
+++ projects/angular-odata/schematics/apigen/files/api-config/__fileName__.ts
@@ -1,11 +1,11 @@
-import { ApiConfig, EDM_PARSERS, ODataMetadata, ODataVersion } from 'angular-odata';
+import { ODataApiConfig, EDM_PARSERS, ODataMetadata, ODataVersion } from 'angular-odata';
 import * as json from './metadata.json';
 
 export const <%= classify(name) %> = ODataMetadata.fromJson(json).toConfig({
-  serviceRootUrl: '<%= serviceRootUrl %>', 
-  metadataUrl: '<%= metadataUrl %>', 
+  serviceRootUrl: '<%= serviceRootUrl %>',
+  metadataUrl: '<%= metadataUrl %>',
   name: '<%= apiConfigName %>',
   version: '<%= version %>' as ODataVersion,
   creation: new Date('<%= creation.toISOString() %>'),
   parsers: EDM_PARSERS
-}) as ApiConfig;
+}) as ODataApiConfig;
