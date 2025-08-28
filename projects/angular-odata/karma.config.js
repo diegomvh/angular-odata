2bf2087d0d430d56a2258001009713301efeb7cb
Diego van Haaster <diegomvh@gmail.com>
2025-08-28 10:50:10 -0300

    D'oh SingleRun = true

--- projects/angular-odata/karma.config.js
+++ projects/angular-odata/karma.config.js
@@ -32,6 +32,7 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadless'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
